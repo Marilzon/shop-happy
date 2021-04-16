63085aa1cafb8dbf1e9cf0b1e87e4fde65fd88ed
Marilzon <maxmaril@hotmail.com>
2021-04-16 20:09:53 -0300

    Feature: API fetched done!

--- js/main.js
+++ js/main.js
@@ -1,7 +1,48 @@
-const products = fetch("server/api.json")
-  .then(response => response.json())
-  .then(data => {
-    console.log(data);
-  });
+const rootProducts = document.querySelector("#root-products");
 
-console.log(products);
+const getAPI = (url) => {
+  fetch(url)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    for (i = 0;i < data.products.length; i++) {
+      rootProducts.insertAdjacentHTML('beforeend',
+        `
+          <div class="card-item">
+            <figure>
+            <img src="${data.products[i].image}" alt="Product image">
+            <figcaption>${data.products[i].name}</figcaption>
+            <p>
+              ${data.products[i].description}
+            </p>
+            <smal>de R$${data.products[i].oldPrice.toFixed(2)}</smal>
+            <p>Por R$${data.products[i].price.toFixed(2)}</p>
+            <small>ou ${data.products[i].installments.count} x de R$${data.products[i].installments.value.toFixed(2)}</small>
+            </figure>
+          </div>
+        `
+      );
+      console.log(data.products[i].name);
+    }
+  })
+}
+
+getAPI("https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1");
+
+/*
+const productInterface = `
+  <div class="card-item">
+    <figure>
+      <img src="#" alt="Product image">
+      <figcaption>${data.products[i].name}</figcaption>
+      <p>
+        ${data.products[i].description}
+      </p>
+      <smal>de R$${data.products[i].oldPrice.toFixed(2)}</smal>
+      <p>Por R$${data.products[i].price.toFixed(2)}</p>
+      <small>ou ${data.products[i].installments.count} x de R$${data.products[i].installments.valuetoFixed(2)}</small>
+    </figure>
+  </div>
+`;
+*/
